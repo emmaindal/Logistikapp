23fb19f71f38c0556252e59d61defc4ab16b0bbc
Charlie Olsson <charlieolsson@Charlies-MacBook-Pro.local>
2018-10-24 12:58:22 +0200

    basic login form

--- client/src/views/login/Login.jsx
+++ client/src/views/login/Login.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
+
+//import MuiThemeProvider from '@material-ui/styles/MuiThemeProvider';
+import {AppBar, Button, TextField, Input} from '@material-ui/core';
+
 const Login = () => {
-  return <div>login comp</div>;
+  return (
+    <div>
+      <div>
+        <Input
+          id="standard"
+          placeholder="Username"
+          //className={classes.textField}
+          autoComplete="current-password"
+          margin="normal"
+        />
+        <br />
+        <Input
+          id="standard-password-input"
+          placeholder="Password"
+          //className={classes.textField}
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+        />
+        <br />
+        <Button variant="contained" color="blue">
+          Primary
+        </Button>
+      </div>
+    </div>
+  );
 };
+
+const style = {
+  margin: 15,
+}
 
 export default Login;
--- client/src/views/login/LoginContainer.jsx
+++ client/src/views/login/LoginContainer.jsx
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 import Login from "./Login";
 
 class LoginContainer extends Component {
+
+  state = {
+    username: '',
+    password: '',
+    showPassword: 'false'
+  }
+
   render() {
     return <Login />;
   }
